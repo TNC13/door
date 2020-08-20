2ff9730546b5964d582e7a4a711fb736307cec96
Chicodemilo <mileschick@gmail.com>
2020-08-20 15:56:19 -0500

    Testing connection

--- access.php
+++ access.php
@@ -1,18 +1,23 @@
 <html>
- <body>
-  <head>
-   <title>
-     run
-   </title>
-  </head>
 
-   <form action="./run.php" method="post">
-    <input type="submit" value="OPEN" name="GO">
-   </form>
+<body>
 
-   <form action="./close.php" method="post">
-    <input type="submit" value="CLOSE" name="GO">
-   </form>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="dog.css">
+        <title>
+            🐶Door
+        </title>
+    </head>
 
- </body>
+    <form action="./run.php" method="post">
+        <input type="submit" value="OPEN" name="GO">
+    </form>
+
+    <form action="./close.php" method="post">
+        <input type="submit" value="CLOSE" name="GO">
+    </form>
+
+</body>
+
 </html>
