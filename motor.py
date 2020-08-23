74569257dd3c556096fcfeed10d6d61e011cc299
Chicodemilo <mileschick@gmail.com>
2020-08-23 12:59:31 -0500

    timing changes

--- motor.py
+++ motor.py
@@ -4,54 +4,56 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-control_pins = [7,11,13,15]
-pins_two = [29,31,33,35]
+control_pins = [7, 11, 13, 15]
+pins_two = [29, 31, 33, 35]
 
 for pin in control_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
 
 
 for pin in pins_two:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
 
 backwards = [
-  [1,0,0,1],
-  [0,0,0,1],
-  [0,0,1,1],
-  [0,0,1,0],
-  [0,1,1,0],
-  [0,1,0,0],
-  [1,1,0,0],
-  [1,0,0,0],
+    [1, 0, 0, 1],
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
 ]
 
 halfstep_seq = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1]
 ]
 
 
 def openOne():
-  for i in range(265):
-    for halfstep in range(8):
-      for pin in range(4):
-       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-      time.sleep(0.001)
+    for i in range(350):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            time.sleep(0.001)
+
 
 def openTwo():
-  for i in range(265):
-    for halfstep in range(8):
-      for pin in range(4):
-       GPIO.output(pins_two[pin], backwards[halfstep][pin])
-      time.sleep(0.001)
+    for i in range(350):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(pins_two[pin], backwards[halfstep][pin])
+            time.sleep(0.001)
+
 
 openOne()
 openTwo()
